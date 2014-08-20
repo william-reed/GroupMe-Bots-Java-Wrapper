3404d5d471c4488de31f04e92745f5e98ae473c4
William Reed <wreed58@bitbucket.org>
2014-08-20 15:13:21 -0700

    Added builds folder

--- src/github/io/wreed12345/GroupMeAPI.java
+++ src/github/io/wreed12345/GroupMeAPI.java
@@ -2,10 +2,12 @@ package github.io.wreed12345;
 
 public final class GroupMeAPI
 {
+	public static final String VERSION = "0.0.1";
+	
 	//just a test
 	public static void main(String[] args)
 	{
-		Bot bot = new Bot("19f76e34578b8da260a59d3819");
+		Bot bot = new Bot("your id here");
 		bot.sendTextMessage("Test message");
 		bot.sendImage("Check out this image", "http://www.wtt.com/Pictures/WTT_logo_color_Jan2010.gif");
 		bot.sendLocation("Home", 38.8977, -77.0366, "The White House");
